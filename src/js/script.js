848fa7bec82ea9be93ae5b313af507ea2d664a90
Egor <egor91001@mail.ru>
2019-05-07 01:08:35 +0300

    Пятый(2) коммит

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+let start = document.getElementById('start'),
+
+    budgetV = document.getElementsByClassName('budget-value')[0],
+    daybudgetV = document.getElementsByClassName('daybudget-value')[0],
+    levelV = document.getElementsByClassName('level-value')[0],
+    expensesV = document.getElementsByClassName('expenses-value')[0],
+    optionalexpensesV = document.getElementsByClassName('optionalexpenses-value')[0],
+    incomeV = document.getElementsByClassName('income-value')[0],
+    monthsavingsV = document.getElementsByClassName('monthsavings-value')[0],
+    yearsavingsV = document.getElementsByClassName('yearsavings-value')[0],
+    expensesItem = document.getElementsByClassName('expenses-item')[0, 1, 2, 3],
+
+    expensesItemBtn = document.getElementsByName('expenses-item-btn'),
+    optionalexpensesBtn = document.getElementsByName('optionalexpenses-btn'),
+
+    optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item')[0, 1, 2],
+
+
+    chooseIncomeLabel = document.querySelector('.choose-income-label'),
+    checksavings = document.querySelector('.checksavings'),
+    sum = document.querySelector('.sum'),
+    percent = document.querySelector('.percent'),
+    year = document.querySelector('.year'),
+    month = document.querySelector('.month'),
+    day = document.querySelector('.day');
+
+console.log(expensesItem);
