da2367637a8d90af2cf9da67496f0851f80c7432
Egor <egor91001@mail.ru>
2019-05-21 23:11:07 +0300

    Восьмой коммит

--- lesson_7.1/script.js
+++ lesson_7.1/script.js
@@ -34,4 +34,49 @@ window.addEventListener('DOMContentLoaded', function() {
         }
 
     });
+
+    // Timer
+
+    let deadline = '2019-5-22';
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+            
+        if (hours < 10) hours = "0" + hours;
+        if (minutes < 10) minutes = "0" + minutes;
+        if (seconds < 10) seconds = "0" + seconds;
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endTime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                hours.textContent = '00'
+                minutes.textContent = '00'
+                seconds.textContent = '00'
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setClock('timer', deadline);
 });
